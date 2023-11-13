67d6eb848e96630f9f9023f5d5ab10b48a891310
jdw1042710 <jdw1042710@gmail.com>
2023-11-13 18:01:21 +0900

    Feat: implement car steering by oculus controller

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +9,14 @@ public class InputManager : MonoBehaviour
 
     public float vertical;
     public float horizontal;
+
+    public bool isDownLeftGrabButton;
+    public bool isDownRightGrabButton;
+
+    public bool isUpLeftGrabButton;
+    public bool isUpRightGrabButton;
+
+    public bool isBrake;
 
     private void Awake()
     {
@@ -21,7 +30,14 @@ public class InputManager : MonoBehaviour
 
     private void Update()
     {
-        vertical = Input.GetAxis("Vertical");
-        horizontal = Input.GetAxis("Horizontal");
+        //vertical = Input.GetAxis("Vertical");
+        //horizontal = Input.GetAxis("Horizontal");
+        vertical = OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger);
+        isBrake = OVRInput.Get(OVRInput.Button.SecondaryHandTrigger);
+
+        isDownLeftGrabButton = OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger);
+        isDownRightGrabButton = OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger);
+        isUpLeftGrabButton = OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger);
+        isUpRightGrabButton = OVRInput.GetUp(OVRInput.Button.SecondaryIndexTrigger);
     }
 }
